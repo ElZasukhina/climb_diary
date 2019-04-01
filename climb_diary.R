7b9e8608ebcd28dc99469f42ed32e294dad1223a
LizaZas <elizaveta.zasukhina@merck.com>
2019-04-01 07:36:52 +0200

    fixed minor bugs; features added: tab menu, frequency graph, filter by date

--- climb_diary.R
+++ climb_diary.R
@@ -20,41 +20,72 @@ data %<>% as.data.table()
 
 #as.Date(DP$variable, origin = "1899-12-30")
 
+#labels function
+labels <- function(x){
+  ifelse(x==4.25, '4+', 
+         ifelse(x==4.5, '4+/5-',
+                ifelse(x== 4.75, '5-', 
+                       ifelse(x== 5.25, '5+', 
+                              ifelse(x==5.5, '5+/6-',
+                                     ifelse(x==5.75, '6-',
+                                            ifelse(x==6.25, '6+',
+                                                   ifelse(x==6.5, '6+/7-',
+                                                          ifelse(x==6.75, '7-',
+                                                                 ifelse(x==7.25, '7+',
+                                                                        x))))))))))
+}
+
 #Interface
 
 ui <- fluidPage(
-    
-    titlePanel("Liza's climbing diary"),
-    sidebarLayout(
-      sidebarPanel(
-        helpText("Insert new record and click 'Send'. Update the page to see new data"),
-        
-        h3('New record'),
-        dataTableOutput('new_record'),
-        #textInput('name', 'Name'),
-        dateInput('date', 'Date'),
-        sliderInput('grade', 'Grade', 4,8, 5, step= 0.25),
-        selectInput('style', 'Style', c('OS', 'RC'), multiple = F),
-        actionButton("send", "Send"),
+  
+  titlePanel("Liza's climbing diary"),
+  
+  sidebarLayout(
+    sidebarPanel(
       
-        br()
+      helpText("Insert new record and click 'Send'. Update the page to see new data"),
       
-        # h3('Filter'),
-        # dateRangeInput('date', 'Date'),
-        # br()
+      h3('New record'),
+      dataTableOutput('new_record'),
+      #textInput('name', 'Name'),
+      dateInput('date', 'Date'),
+      sliderInput('grade', 'Grade', 4,8, 5, step= 0.25),
+      selectInput('style', 'Style', c('OS', 'TR', 'AF'), multiple = F),
+      actionButton("send", "Send"),
+      
+      br(),
+      br(),
+      
+      h3('Filter'),
+      dateRangeInput('date', 'Date', start = '2019-03-19', weekstart = 1, autoclose = T)
     ),
     
     mainPanel(
-      plotOutput("plot"),
-      tableOutput('table')
+      tabsetPanel(
+        tabPanel("Plot", plotOutput("plot")), 
+        tabPanel("Frequency",  plotOutput('freq')), 
+        tabPanel("Records", 
+                 fluidPage(
+                   fluidRow(
+                     column(6,
+                            h3("Top-10 routes"),
+                            tableOutput('top')),
+                     
+                     column(6,
+                            h3("Last records"),
+                            tableOutput('table')))))
+        
+      )
     )
   )
 )
 
 
+
 # Server
-  
-options(shiny.maxRequestSize = 9*1024^2)
+
+#options(shiny.maxRequestSize = 9*1024^2)
 
 server = function(input, output) {
   
@@ -64,33 +95,41 @@ server = function(input, output) {
                                       as.character(input$style)))
   })
   
-lab <- ifelse(data$Grade==4.25, '4+', 
-              ifelse(data$Grade==4.5, '4+/5-',
-                ifelse(data$Grade== 4.75, '5-', 
-                       ifelse(data$Grade== 5.25, '5+', 
-                              ifelse(data$Grade==5.5, '5+/6-',
-                                     ifelse(data$Grade==5.75, '6-',
-                                            ifelse(data$Grade==6.25, '6+',
-                                                   ifelse(data$Grade==6.5, '6+/7-',
-                                                          ifelse(data$Grade==6.75, '7-',
-                                                                 ifelse(data$Grade==7.25, '7+',
-                                                                 data$Grade))))))))))
-                             
-
- output$plot = renderPlot(
-    ggplot(data, aes(x = Date, y = Grade, col=as.factor(Style))) +
-      geom_jitter(size = 5, width = 0.1, height=0.1, alpha=0.75) +
+  
+  
+  output$plot = renderPlot(
+    ggplot(data[Date %in% as.Date(min(input$date)):as.Date(max(input$date))], aes(x = Date, y = Grade)) + 
+      geom_jitter(aes(col=as.factor(Style)), size = 6, width = 0.1, height=0.1, alpha=0.75) +
       scale_y_continuous(breaks = seq(4, 8, 0.5), limits = c(4, 6)) +
-      geom_text(label=lab, col='cadetblue4') +
+      geom_text(label=labels(data[Date %in% as.Date(min(input$date)):as.Date(max(input$date))]$Grade), 
+                nudge_x = -0.4, size=5, col='grey6') +
       labs(color = 'Style') +
+      scale_color_manual(values=c("tomato", "steelblue3", "slategrey")) +
       theme_minimal()
- )
-  output$table = renderTable(
-    data.table('Date' = as.character(data$Date),
-               'Grade' = lab,
-               'Style' = as.character(data$Style))[order(-Date)]
   )
-
+  
+  output$top = renderTable(
+    head(data[,Route := as.character(labels(Grade))][order(-Grade)][,-2][, Date := as.character(data$Date)], 10)
+  )
+  
+  #mb write here last 20 records? mb convert data into date format here
+  output$table = renderTable(
+    data.table('Date' = data$Date, #as.character(data$Date)
+               'Grade' = labels(data$Grade),
+               'Style' = as.character(data$Style))[
+                 Date %in% as.Date(min(input$date)):as.Date(max(input$date)),
+                 ][order(-Date)][,Date := as.character(Date)]
+  )
+  
+  output$freq = renderPlot(
+    ggplot(data[Date %in% as.Date(min(input$date)):as.Date(max(input$date))], aes(Date)) +
+      geom_bar(fill = 'steelblue3', alpha=0.75, width = 0.7) +
+      labs(y = '# of routes') +
+      scale_y_continuous(breaks = seq(1, 10, 1)) +
+      #geom_text(aes(y=count(Date))) +
+      theme_minimal()
+  )
+  
 }
 
 
